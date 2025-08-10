aeb086270480a137341a4c97807c8be75ba241be
Assembling Kings <assembling.kings@outlook.com>
2025-08-10 14:30:13 -0400

    Initial `Talent*` implementation

---
+++ src/sheet/items/advancement/talent/TalentApp.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import { inject, computed } from "vue";
+import { ContextSymbol } from "@/sheet/VueApplicationMixin";
+import { type TalentContext } from "@/sheet/items/advancement/talent/TalentSheet";
+import { AbilityActivation } from "@/values/Activation";
+
+const context = inject<TalentContext>(ContextSymbol)!;
+const system = computed(() => context.document.system);
+
+const activationOptions = Object.values(AbilityActivation).map((activation) => [
+   game.i18n.localize(`GENESYS.ability.activation.${activation}`), activation,
+]);
+</script>
+
+<template>
+   <header class="talent-header">
+      <img :src="context.document.img"  data-edit="img" data-action="editImage"
+         :title="context.document.name" :alt="context.document.name" />
+      <input type="text" :value="context.document.name" name="name" />
+   </header>
+
+   <section class="talent-content">
+      <input type="number" :value="system.tier" name="system.tier" />
+      <input type="number" :value="system.rank" name="system.rank" />
+      <input type="number" :value="system.uses.max" name="system.uses.max" />
+      <select :value="system.activation" name="system.activation">
+         <option v-for="[label, value] in activationOptions" :key="value" :value="value">
+         {{ label }}
+         </option>
+      </select>
+
+      <prose-mirror :value="system.description" name="system.description" />
+      <input  type="text" :value="system.source" name="system.source" />
+   </section>
+
+</template>
+
+<style>
+   .talent-header {
+      margin-bottom: 1em;
+   }
+
+   .talent-content {
+      display: flex;
+      flex-direction: column;
+      gap: 1em;
+   }
+</style>
---
+++ src/sheet/items/advancement/talent/TalentModel.ts
@@ -0,0 +1,26 @@
+import { AbilityModel } from "@/sheet/items/advancement/ability/AbilityModel";
+
+export class TalentModel extends AbilityModel {
+   declare tier: number;
+   declare rank: number;
+
+   static override defineSchema(): foundry.abstract.types.DataSchema {
+      const { NumberField } = foundry.data.fields;
+      return {
+         ...super.defineSchema(),
+         tier: new NumberField({
+            initial: 1,
+            integer: true,
+            min: 1,
+            max: 5,
+            nullable: false,
+         }),
+         rank: new NumberField({
+            initial: 0,
+            integer: true,
+            min: 0,
+            nullable: false,
+         }),
+      };
+   }
+}
